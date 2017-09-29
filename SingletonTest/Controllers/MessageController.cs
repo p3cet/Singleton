37fb84b3bf4aa017185a393d45144da7f9678fe3
p3cet <cprzem@gmail.com>
2017-09-29 21:10:37 +0200

    Add files via upload

---
+++ SingletonTest/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using SingletonTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingletonTest.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public string Index()
+        {
+            Message CurrentMessage = Message.Instance;
+            return "Message initialized!";
+        }
+
+        public string ViewMessage()
+        {
+            return Message.MessageText;
+        }
+
+        public string SetMessage(string text)
+        {
+            Message.MessageText = text;
+            return "Message set to: " + Message.MessageText;
+        }
+    }
+}
---
+++ SingletonTest/Models/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingletonTest.Models
+{
+    public class Message
+    {
+        public static Message Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Message();
+                return instance;
+            }
+        }
+
+        public static string MessageText
+        {
+            get => messageText;
+            set => messageText = value;
+        }
+
+        private static Message instance;
+        private static string messageText;
+        private Message() { }
+
+
+    }
+}
